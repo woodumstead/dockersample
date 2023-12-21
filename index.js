b9d67ae318e2b07dbc0df9fcbe7cf19e557fc17c
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-12-21 06:53:50 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const app  = require('express')();
 
+// -- get route
 app.get('/', (req, res) => {
     res.json({ message: 'docker sample' })
+});
+
+/ -- post route
+app.post('/newroute/:id', (req, res) => {
+    const { id } = req.params;
+    const { size } = req.body;
+
+    if (!newroute) {
+        res.status(404).send({ message: 'page not found' })
+    }
+
+    res.send({
+        message: `new route`
+    });
 });
 
 const port = process.env.PORT || 1941;
